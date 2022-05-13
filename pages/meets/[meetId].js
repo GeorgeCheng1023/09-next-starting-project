9cef665d96b2c8a623f9b45924a2e3c934857490
GeorgeCheng1023 <ck40292@gmail.com>
2022-05-13 13:23:15 +0800

    .

--- pages/meets/[meetId].js
+++ pages/meets/[meetId].js
@@ -23,7 +23,7 @@ const DetailPage = () => {
               Some quick example text to build on the card title and make up the
               bulk of the card's content.
             </Card.Text>
-            <Button variant="primary" onClick={goBackHandler}>
+            <Button bsPrefix="custom-btn" onClick={goBackHandler}>
               Back
             </Button>
           </Card.Body>
