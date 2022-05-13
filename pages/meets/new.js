309f6b425d3ae08b6ce87cc94c2bc73136f8fc67
George Cheng <ck40292@gmail.com>
2022-05-13 13:40:36 +0800

    feat: new meet api

--- pages/meets/new.js
+++ pages/meets/new.js
@@ -1,9 +1,21 @@
 import NewMeetsupForm from "../../components/meetups/NewMeetupForm";
 
-const NewMeetsPage = () => {
+const NewMeetsPage = (props) => {
+  const addMeetupHandler = async (enteredMeetup) => {
+    const response = await fetch("/api/new-meetsup", {
+      method: "POST",
+      body: JSON.stringify(enteredMeetup),
+      header: {
+        "Content-Type": "application/json",
+      },
+    });
+    const data = await response.json();
+    console.log(data);
+  };
+
   return (
     <>
-      <NewMeetsupForm />
+      <NewMeetsupForm onAddMeetup={addMeetupHandler} />
     </>
   );
 };
